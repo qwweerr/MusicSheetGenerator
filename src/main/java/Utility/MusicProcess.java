3d62e46d206b81e60cb71ff3c8612624b24dbd04
qwweerr <robertzhan92@hotmail.com>
2020-08-20 16:14:05 -0400

    create mp3 analyze constractor

--- src/main/java/Utility/MusicProcess.java
+++ src/main/java/Utility/MusicProcess.java
@@ -1,12 +1,23 @@
 package Utility;
 
-import javax.sound.*;
+import org.tritonus.share.sampled.TAudioFormat;
+import org.tritonus.share.sampled.file.TAudioFileFormat;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.*;
 
 public class MusicProcess {
+    AudioFileFormat baseFileFormat;
+    AudioFormat baseFormat;
 
+    public MusicProcess(String FilePath) throws IOException, UnsupportedAudioFileException {
+        File file = new File(FilePath);
+        baseFileFormat = AudioSystem.getAudioFileFormat(file);
+        baseFormat = baseFileFormat.getFormat();
+    }
 }
-
-
 
 
